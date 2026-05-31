fd16692a42689824ac6408dee6dcb2b7fa80c47e
Uellington Santos <uellington.santos@outlook.com>
2026-05-31 08:25:31 -0300

    Enable forced password change middleware

--- myrobot_checklist/settings.py
+++ myrobot_checklist/settings.py
@@ -29,6 +29,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'checklists.middleware.ForcePasswordChangeMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
