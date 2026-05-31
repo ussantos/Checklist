8ff359722fa26f64d5edae5491bece8e4ff554ea
Uellington Santos <uellington.santos@outlook.com>
2026-05-31 08:55:53 -0300

    Merge conflicting checklist migrations

---
+++ checklists/migrations/0004_merge_userprofile_password_and_nominal_registration.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('checklists', '0002_userprofile_must_change_password'),
+        ('checklists', '0003_nominal_employee_registration'),
+    ]
+
+    operations = []
